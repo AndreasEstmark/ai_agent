1d4532c4738bfa7233071dfad099921091c04544
Anden <73430931+AndreasEstmark@users.noreply.github.com>
2025-08-31 08:44:20 +0200

    testing with non local as well

--- src/qwen_agent.py
+++ src/qwen_agent.py
@@ -6,7 +6,7 @@ from pydantic_ai import Agent
 from pydantic_ai.common_tools.duckduckgo import duckduckgo_search_tool
 
 ollama_model = OpenAIModel(
-    model_name="qwen3:1.7b",
+    model_name="qwen3:1.7b", #Only model i could run locally. 
     provider=OpenAIProvider(base_url="http://localhost:11434/v1"),
 )
 agent = Agent(
